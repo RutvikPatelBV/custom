4d0e6f7e98b089d5cc13c0320a0f448a4810f800
RutvikBV <brainviretest2228@gmail.com>
2024-05-03 18:42:55 +0530

    complete fetch default selected order line

--- project_management/wizard/customize_sale_print_wizard.py
+++ project_management/wizard/customize_sale_print_wizard.py
@@ -4,15 +4,21 @@ from odoo import fields, models, api
 class CustomizeSalePrintWizard(models.TransientModel):
     _name = 'customize.sale.print.wizard'
     _description = 'customize sale print wizard'
-    # _inherit = 'sale.order'
-    priority = fields.Selection([('0', 'Low'), ('1', 'Normal'), ('2', 'High'), ('3', 'Very High'), ('4', 'Urgent')],
-                                string="Project Priority")
-    # product=self.env['sale.order.line'].get.context('active_id').
-    # order_line = fields.One2many(
-    #     comodel_name='sale.order.line',
-    #     inverse_name='order_id',
-    #     string="Order Lines",
-    #     copy=True, auto_join=True)
+    order_lines = fields.Many2many(
+        comodel_name='sale.order.line',
+        string="Order Lines",
+    )
+
+    @api.model
+    def default_get(self, fields):
+        res = super(CustomizeSalePrintWizard, self).default_get(fields)
+        active_id = self._context.get('active_id')
+        if active_id:
+            sale_order = self.env['sale.order'].browse(active_id)
+            res['order_lines'] = [(6, 0, sale_order.order_line.ids)]
+        return res
+
+
     def print_report(self):
-        self.env['pms.project'].browse(self._context.get('active_id')).update({'priority': self.priority})
-        return True
+           pass
+
