a3cff69325cd06c36262fe4b93a876a82cd8acd6
RutvikPatelBV <rutvik.patel@brainvire.com>
2024-06-06 18:54:28 +0530

    Complete public widget task in that i extends PurchasePortalSidebar by include and add custom functionality in that

--- Practice/__manifest__.py
+++ Practice/__manifest__.py
@@ -6,7 +6,7 @@
     'description': 'For Practice Access Rights',
     'author': 'RP',
     'license': '',
-    'depends': ['base', 'mail', 'web', 'hr_expense'],
+    'depends': ['base', 'mail', 'web', 'hr_expense', 'purchase', 'website'],
     'data': ['security/ir.model.access.csv',
              'data/data.xml',
              'data/new_template.xml',
@@ -18,15 +18,19 @@
              "views/equipment_view.xml",
              "views/equipment_order_view.xml",
              "views/equipment_delivery_view.xml",
+             "views/purchase_portal_sidebar_inherite.xml",
              "report/hr_expense_qweb_report.xml",
              ],
     'assets': {
         'web.assets_backend': [
-            'Practice/static/src/**/*',
-            # 'Practice/static/src/views/hr_expense_custom_button.xml',
-            # 'Practice/static/src/views/hr_expense_custom_button.xml',
+            # 'Practice/static/src/**/*',
+            'Practice/static/src/components/hr_expense_custom_button_action.js',
+            'Practice/static/src/views/hr_expense_custom_button.xml',
 
-        ]
+        ],
+        'web.assets_frontend': [
+            'Practice/static/src/js/purchase_portal_sidebar_inheritance.js',
+        ],
     },
 
     'demo': [''],
---
+++ Practice/static/src/js/purchase_portal_sidebar_inheritance.js
@@ -0,0 +1,33 @@
+/** @odoo-module **/
+import publicWidget from "@web/legacy/js/public/public_widget";
+import PortalSidebar from '@portal/js/portal_sidebar';
+
+PortalSidebar.include({
+    selector: ".o_portal_purchase_sidebar",
+    template: "purchase.portal_my_home_menu_purchase",
+
+    init: function (parent) {
+        this._super.apply(this, arguments);
+        console.log("Jay Shree Ram");
+    },
+
+    start: function () {
+        this._super.apply(this, arguments);
+
+        this.$el.css('background-color', '#FFE6E6');
+        this.$el.css('border-radius','20px')
+
+        // Use document to query the DOM correctly
+        let element = document.querySelector(".col-lg-3.col-xl-4.d-print-none");
+        if (element) {
+            element.style.backgroundColor = '#E1AFD1';
+            element.style.borderTopLeftRadius = '20px';
+            element.style.borderBottomLeftRadius = '20px';
+        } else {
+            console.warn("Element with class 'col-lg-3 col-xl-4 d-print-none' not found");
+        }
+        this.$el.on('click', '#redirect_to_contact_us', function() {
+            window.location.href = '/contactus';
+        });
+    }
+});
