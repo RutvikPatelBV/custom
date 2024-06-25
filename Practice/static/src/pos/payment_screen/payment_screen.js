e9643d7890eaa5125ab580a5402c4d5ee3b0fd75
RutvikPatelBV <rutvik.patel@brainvire.com>
2024-06-25 15:42:56 +0530

    add custom component add prop hooks and add clear all clear one and item count functionality

--- Practice/static/src/pos/payment_screen/payment_screen.js
+++ Practice/static/src/pos/payment_screen/payment_screen.js
@@ -3,12 +3,35 @@ import { _t } from "@web/core/l10n/translation";
 import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
 //import { CustomAlertPopup } from "@pos_buttons/js/PopUp/pos_pop_up";
 import { patch } from "@web/core/utils/patch";
+import {useState} from "@odoo/owl";
+
 patch(PaymentScreen.prototype, {
 setup(){
+this.state=useState({value:0})
 super.setup()
 console.log("Jay Shree Ram From Payment")
 },
 async onClick() {
-alert("Jay Shree Ram From Payment")
+alert(`Jay Shree Ram From Payment ${this.state.value}`)
+this.state.value++
 }
 })
+
+
+//add service which give notification every 5 second:
+
+//import { registry } from "@web/core/registry";
+//
+//const serviceRegistry = registry.category("services");
+//
+//const myService = {
+//    dependencies: ["notification"],
+//    start(env, { notification }) {
+//        let counter = 1;
+//        setInterval(() => {
+//            notification.add(`Tick Tock ${counter++}`);
+//        }, 5000);
+//    }
+//};
+//
+//serviceRegistry.add("myService", myService);
---
+++ Practice/static/src/pos/total_qty.js
@@ -0,0 +1,32 @@
+/** @odoo-module **/
+
+import { OrderWidget } from "@point_of_sale/app/generic_components/order_widget/order_widget"; // Adjust the import path accordingly
+import { patch } from "@web/core/utils/patch";
+import { useState, onPatched, onMounted } from "@odoo/owl";
+
+// Patch the OrderWidget component
+patch(OrderWidget.prototype, {
+    setup() {
+        // Call the original setup method
+        super.setup();
+
+        // Initialize state with useState
+        this.state = useState({ qty: 0 });
+
+        // Function to update the quantity
+        const updateQuantity = () => {
+            const totalQuantity = this.props.lines.reduce((acc, line) => acc + line.quantity, 0);
+            this.state.qty = totalQuantity;
+//            console.log("Total quantity:", this.state.qty);
+        };
+
+        // Use onMounted to update the quantity when the component is first mounted
+        onMounted(() => {F
+            updateQuantity();
+        });
+
+         onPatched(() => {
+            updateQuantity();
+        });
+    },
+});
