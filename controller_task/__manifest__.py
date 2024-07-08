52d7bde65993c215bb5ddccef2d40e5cc2c2fa03
RutvikPatelBV <rutvik.patel@brainvire.com>
2024-07-08 17:54:15 +0530

    create controller for create new sale order whit post method

---
+++ controller_task/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Custom Sale Order Controller',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Module to create sale orders through a custom controller',
+    'description': """
+        This module defines a custom controller to handle form submissions and create sale orders.
+    """,
+    'author': 'Your Name',
+    'depends': ['base', 'sale', 'website'],
+    'data': [
+          # Assuming you have a template file for your form
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ controller_task/controllers/controllers.py
@@ -0,0 +1,31 @@
+from odoo import http
+from odoo.http import request
+
+
+class CustomSaleOrderController(http.Controller):
+
+    @http.route('/create_sale_order', type='json', auth="public", methods=['POST'])
+    def create_sale_order(self, **kwargs):
+        data = request.get_json_data()
+
+        partner_id = data.get('partner_id')
+        order_lines = data.get('order_lines', [])
+
+        if not partner_id or not order_lines:
+            return {"error": "Missing partner_id or order_lines"}
+
+        order_vals = {
+            'partner_id': partner_id,
+            'order_line': [(0, 0, {
+                'product_id': line.get('product_id'),
+                'product_uom_qty': line.get('quantity'),
+                'price_unit': line.get('price'),
+            }) for line in order_lines]
+        }
+
+        sale_order = request.env['sale.order'].sudo().create(order_vals)
+
+        return {
+            "message": "Sale Order Created",
+            "order_id": sale_order.id
+        }
